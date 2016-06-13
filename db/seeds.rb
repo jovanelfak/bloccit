4ba7626158fc8b2e28401896bc7e3ef4b472809d
JohnZiv <jovan.z@elfak.rs>
2016-06-13 15:59:01 -0400

    Adds or finds a unique post

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,9 @@ require 'random_data'
      body: RandomData.random_paragraph
    )
  end
- 
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "A unique title", body: "A unique body")
+ puts "#{Post.count}"
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
