5ea818b4ace010a4db928fe3b9e42ea394c96143
Xander Miller <xander.miller@gmail.com>
2016-07-05 18:14:21 +0000

    cleans things up abit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,22 +11,18 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
- # #9
-     @post = Post.new
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
 
- # #10
-     if @post.save
- # #11
-       flash[:notice] = "Post was saved successfully."
-       redirect_to @post
-     else
- # #12
-       flash.now[:alert] = "There was an error saving the post. Please try again."
-       render :new
-     end
-   end
+    if @post.save
+      flash[:notice] = "Post was saved successfully."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
 
   def edit
   end
