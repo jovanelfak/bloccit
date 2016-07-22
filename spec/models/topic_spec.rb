b0105755818f98033d5d379421cd94cd20f1f6e7
JohnZiv <jovan.z@elfak.rs>
2016-07-22 13:35:17 -0400

    First commit

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Topic, type: :model do
    let(:public) { true }
    let(:topic) { Topic.create!(name: name, description: description) }
 
+    it { should validate_length_of(:name).is_at_least(5)}
+    it { should validate_length_of(:description).is_at_least(15)}
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:description) }
 
    it { is_expected.to have_many(:posts) }
  # #1
