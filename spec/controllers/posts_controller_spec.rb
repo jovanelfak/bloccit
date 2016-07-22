c463b3ae89fc680107ded817a91e49a873d75ccf
JohnZiv <jovan.z@elfak.rs>
2016-07-22 16:32:54 -0400

    Syntax errors fixed !

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,18 +3,6 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-    it "assigns [my_post] to @posts" do
-      my_post
-       get :index
- # #9
-       expect(assigns(:posts)).to eq([my_post])
-     end
-  end
 
  describe "GET new" do
       it "returns http success" do
