c25b6270fd3352cbd8fed229dfba0e822a848f9e
JohnZiv <jovan.z@elfak.rs>
2016-07-29 10:38:51 -0400

    First commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User < ActiveRecord::Base
 
-	before_save { self.email = email.downcase if email.present? }
+  before_save { self.name = name.split.map(&:capitalize).join(' ') if name.present? }
+  before_save { self.email = email.downcase if email.present? }
 
- # #3
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
- # #4
-  validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
-  validates :password, length: { minimum: 6 }, allow_blank: true
- # #5
-   validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
- # #6
-   has_secure_password
+  validates :password_digest, presence: true, length: { minimum: 6 }, unless: :password_digest
+  validates :password_digest, length: { minimum: 6 }, allow_blank: true
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 254 }
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,39 +2,40 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-   # Shoulda tests for name
-   it { is_expected.to validate_presence_of(:name) }
-   it { is_expected.to validate_length_of(:name).is_at_least(1) }
- 
-   # Shoulda tests for email
-   it { is_expected.to validate_presence_of(:email) }
-   it { is_expected.to validate_uniqueness_of(:email) }
-   it { is_expected.to validate_length_of(:email).is_at_least(3) }
-   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
- 
-   # Shoulda tests for password
-   it { is_expected.to validate_presence_of(:password) }
-   it { is_expected.to have_secure_password }
-   it { is_expected.to validate_length_of(:password).is_at_least(6) }
- 
-   describe "attributes" do
-     it "should have name and email attributes" do
-       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
-     end
-   end
+  describe 'attributes' do
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:email).of_type(:string) }
+    it { should have_db_column(:password_digest).of_type(:string) }
+  end
+  
+  describe 'associations' do
+  end
+  
+  describe 'validation' do
+    # Name Test
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1) }
+  
+    # Email Test
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_length_of(:email).is_at_least(3) }
+    it { should allow_value("user@bloccit.com").for(:email) }
+  
+    # Password Test
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password}
+    it { should validate_length_of(:password_digest).is_at_least(6) }
+  end
+  
+  describe 'instance methods' do
+    let(:user) {User.create!(name: "bloccit user", email: "user@bloccit.com", password_digest: "password") }
 
-   describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
- 
-     it "should be an invalid user due to blank name" do
-       expect(user_with_invalid_name).to_not be_valid
-     end
- 
-     it "should be an invalid user due to blank email" do
-       expect(user_with_invalid_email).to_not be_valid
-     end
- 
-   end
+    describe "fix user name to proper format" do
+      it "should return the proper fomated name" do
+        expect(user.name).to eq("Bloccit User")
+      end
+    end
+  end
+
 end
