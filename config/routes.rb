369e45e0af56f98ac386df41b011e6d5ca98495c
JohnZiv <jovan.z@elfak.rs>
2016-07-29 10:57:28 -0400

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-get 'about', to: 'welcomes#about'
-get 'about', to: 'welcomes#index'
 
   resources :topics do
     resources :posts, except: [:index]
   end
 
   resources :users, only: [:new, :create]
+
+  get 'about' => 'welcomes#about'
+
+  root 'welcomes#index'
 
 end
