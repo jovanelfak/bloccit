b82b745e74fecf13176727ecd74f77ccd56fc2a6
Xander Miller <xander.miller@gmail.com>
2016-07-29 13:51:07 +0000

    fixes welcomes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-get 'about' => 'welcome#about'
+  root 'welcomes#index'
+  get 'about', to: 'welcomes#about'
 
-root 'welcome#index'
 
   resources :topics do
     resources :posts, except: [:index]
