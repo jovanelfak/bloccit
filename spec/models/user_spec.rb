d887da0c68cbe20965a3a6d56fa565466c3fd9e5
JohnZiv <jovan.z@elfak.rs>
2016-08-03 16:11:42 -0400

    Doesn't change anything

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe User, type: :model do
    it { is_expected.to validate_length_of(:password).is_at_least(6) }
  
    describe "attributes" do
+
      it "should have name and email attributes" do
        expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
      end
