01f309097f35b9c3a20453db0d55e31df0044cd0
JohnZiv <jovan.z@elfak.rs>
2016-08-04 16:49:38 -0400

    First commit

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,12 +3,15 @@ require 'random_data'
 
 RSpec.describe Comment, type: :model do
 
-
-
 	let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld")}
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user) }
-  let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
+	let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+	let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user) }	
+	let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
+ 
+     it { is_expected.to belong_to(:post) }
+     it { is_expected.to belong_to(:user) }
+     it { is_expected.to validate_presence_of(:body) }
+     it { is_expected.to validate_length_of(:body).is_at_least(5) }
 
 
   describe "attribute" do
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,18 +1,32 @@
 require 'rails_helper'
+include RandomData
 
-RSpec.describe Comment, type: :model do
+RSpec.describe Post, type: :model do
 
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)}
-  let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
 
+  it { should have_many(:comments) }
+
+  it { should belong_to(:topic) }
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+  it { should validate_presence_of(:user) }
+
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
-    it { should have_db_column(:body).of_type(:text) }
-  end
-  
-  describe 'associations' do
-    it { should belong_to(:post) }
+
+    it "should respond to title" do
+      expect(post).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(post).to respond_to(:body)
+    end
   end
 end
