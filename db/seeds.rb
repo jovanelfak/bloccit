e61d2ca5f716b84a1ac6cdc4bf97cd3ee5b9c83d
JohnZiv <jovan.z@elfak.rs>
2016-08-06 15:01:51 -0400

    Fixes seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,64 @@
-require 'random_data'
+include RandomData
 
- 5.times do
-   User.create!(
- # #3
-   name:     RandomData.random_name,
-   email:    RandomData.random_email,
-   password: RandomData.random_sentence
-   )
- end
- admin = User.create!(
-   name:     'Admin User',
-   email:    'admin@example.com',
-   password: 'helloworld',
-   role:     'admin'
- )
- 
- member = User.create!(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
+#Create Users
+
+  5.times do
+    user = User.create!(
+      name:     RandomData.random_name,
+      email:    RandomData.random_email,
+      password: RandomData.random_sentence
+    )
+  end
+  users = User.all
+# Create topics
+  15.times do
+    Topic.create!(
+      name:         RandomData.random_sentence,
+      description:  RandomData.random_paragraph
+    )
+  end
+  topics = Topic.all
 
 
- 15.times do
-   Topic.create!(
-     name:         RandomData.random_sentence,
-     description:  RandomData.random_paragraph
-   )
- end
- topics = Topic.all
+# Create posts
+  50.times do
 
-50.times do
-	Post.create!(
-    user: users.sample,
-		topic: topics.sample,
-		title: RandomData.random_sentence,
-		body: RandomData.random_paragraph
-		)	
-end
-posts = Post.all
+    Post.create!(
+      user:  users.sample,
+      topic: topics.sample,
+      title: RandomData.random_sentence,
+      body:  RandomData.random_paragraph
+    )
+  end
+  posts = Post.all
 
+  #Create comments
+
+  100.times do
+    Comment.create!(
+
+      post: posts.sample,
+      body:  RandomData.random_paragraph
+    )
+  end
+
+  # Create an admin user
+  admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+  )
+
+  #Create a member
+  member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+  )
 
 puts "Seed finished"
-puts "#{User.count} users created"
-puts "#{Topic.count} topics created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{User.count} users created."
+puts "#{Topic.count} topics created."
+puts "#{Post.count} posts created."
+puts "#{Comment.count} comments created."
