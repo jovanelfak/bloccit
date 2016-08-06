08dcec62e025921f796faaca0919312d05d8cf54
JohnZiv <jovan.z@elfak.rs>
2016-08-05 21:18:06 -0400

    First commit

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,16 +1,25 @@
-class FavoriteMailer < ApplicationMailer
-	default from: "jovan.z@elfak.rs"
+class FavoriteMailer < ActionMailer::Base
+  default from: "brandon.m.wieber@gmail.com"
 
-	def new_comment(user, post, comment)
- 
-     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-     headers["References"] = "<post/#{post.id}@your-app-name.example>"
- 
-     @user = user
-     @post = post
-     @comment = comment
- 
-     mail(to: user.email, subject: "New comment on #{post.title}")
-   end
+  def new_comment(user, post, comment)
+    headers["Message-ID"] = "<comments/#{comment.id}@http://localhost:3000/>"
+    headers["In-Reply-To"] = "<post/#{post.id}@http://localhost:3000/>"
+    headers["References"] = "<post/#{post.id}@http://localhost:3000/"
+
+    @user = user
+    @post = post
+    @comment = comment
+
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@http://localhost:3000/>"
+    headers["In-Reply-To"] = "<post/#{post.id}@http://localhost:3000/>"
+    headers["References"] = "<post/#{post.id}@http://localhost:3000/>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "New favorite on #{post.title}.")
+  end
 end
