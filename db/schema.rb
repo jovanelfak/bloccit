2060b1a0279b55ac8f359f0546373d2c095611a6
JohnZiv <jovan.z@elfak.rs>
2016-08-08 16:32:23 -0400

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20160804211954) do
+
+ActiveRecord::Schema.define(version: 20160805175842) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
