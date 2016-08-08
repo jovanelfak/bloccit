45458015194bd4dca310dc7adee84c2ed1111bef
JohnZiv <jovan.z@elfak.rs>
2016-08-08 16:47:58 -0400

    First commit

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,27 +1,37 @@
 class VotesController < ApplicationController
 
-	   before_action :require_sign_in
+  before_action :require_sign_in
+  respond_to :html, :js
+
   def up_vote
-     update_vote(1)
-     redirect_to :back
-   end
+    update_vote(1)
 
-   def down_vote
-     update_vote(-1)
-     redirect_to :back
-   end
- 
-   private  
-   def update_vote(new_value)
-     @post = Post.find(params[:post_id])
-     @vote = @post.votes.where(user_id: current_user.id).first
- 
-     if @vote
-       @vote.update_attribute(:value, new_value)
-     else
-       @vote = current_user.votes.create(value: new_value, post: @post)
-     end
-   end
+    respond_to do |format|
+      format.js { render :action => "update_vote" }
+    end
 
+  end
+
+  def down_vote
+    update_vote(-1)
+
+    respond_to do |format|
+      format.js { render :action => "update_vote" }
+    end
+  end
+
+  private
+
+  def update_vote(new_value)
+    @post = Post.find(params[:post_id])
+    @vote = @post.votes.where(user_id: current_user.id).first
+
+    if @vote
+      @vote.update_attribute(:value, new_value)
+    else
+      @vote = current_user.votes.create(value: new_value, post: @post)
+    end
+
+  end
 
 end
