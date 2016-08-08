418daf820a9343d98fbe12b7436b521f935d0aff
JohnZiv <jovan.z@elfak.rs>
2016-08-08 11:05:52 -0400

    First commit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,15 +1,19 @@
 class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_admin, only: [:new, :create, :destroy]
-  before_action :authorize_edit, only: [:edit, :update]
+  before_action :authorize_user, except: [:index, :show]
 
   def index
-    @topics = Topic.all
+    @topics = Topic.visible_to(current_user)
   end
 
   def show
     @topic = Topic.find(params[:id])
+
+    unless @topic.public || current_user
+      flash[:error] = "You must be signed in to view private topics."
+      redirect_to new_session_path
+    end
   end
 
   def new
@@ -23,7 +27,7 @@ class TopicsController < ApplicationController
       @topic.labels = Label.update_labels(params[:topic][:labels])
       redirect_to @topic, notice: "Topic was saved successfully."
     else
-      flash.now[:alert] = "Error creating topic. Please try again."
+      flash[:error] = "Error creating topic. Please try again."
       render :new
     end
   end
@@ -39,10 +43,10 @@ class TopicsController < ApplicationController
 
     if @topic.save
       @topic.labels = Label.update_labels(params[:topic][:labels])
-      flash[:notice] = "Topic was updated"
+      flash[:notice] = "Topic was updated."
       redirect_to @topic
     else
-      flash.now[:alert] = "Error saving topic. Please try again"
+      flash[:error] = "Error saving topic. Please try again."
       render :edit
     end
   end
@@ -54,29 +58,19 @@ class TopicsController < ApplicationController
       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
       redirect_to action: :index
     else
-      flash.now[:alert] = "There was an error deleting the topic."
+      flash[:error] = "There was an error deleting the topic."
       render :show
     end
   end
-
-  private
 
   def topic_params
     params.require(:topic).permit(:name, :description, :public)
   end
 
-  def authorize_admin
+  def authorize_user
     unless current_user.admin?
-      flash[:alert] = "You must be an admin to do that"
+      flash[:error] = "You must be an admin to do that."
       redirect_to topics_path
     end
   end
-
-  def authorize_edit
-    unless current_user.admin? || current_user.moderator?
-      flash[:alert] = "You must be a moderator or admin to do that"
-      redirect_to topics_path
-    end
-  end
-
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,14 +5,23 @@ RSpec.describe Topic, type: :model do
   let(:topic) { create(:topic) }
 
   it { should have_many(:posts) }
-  it { is_expected.to have_many(:labelings) }
-  it { is_expected.to have_many(:labels).through(:labelings) }
+  it { should have_many(:labelings) }
+  it { should have_many(:labels).through(:labelings) }
 
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15)}
 
   describe "attributes" do
-   it "responds to name and description attributes" do
-       expect(topic).to have_attributes(name: topic.name, description: topic.description)
-     end
+    it "should respond to name" do
+      expect(topic).to respond_to(:name)
+    end
+
+    it "should respond to description" do
+      expect(topic).to respond_to(:description)
+    end
 
     it "should respond to public" do
       expect(topic).to respond_to(:public)
@@ -20,6 +29,24 @@ RSpec.describe Topic, type: :model do
 
     it "should be public by default" do
       expect(topic.public).to be(true)
+    end
+  end
+
+  describe "scopes" do
+    before do
+      @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = User.new
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+
+      it "returns only public topics if user is nil" do
+        expect(Topic.visible_to(nil)).to eq([@public_topic])
+      end
     end
   end
 end
