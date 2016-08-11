5103c7ed40c2eac5c2a648f191a61eb54a4ce6ae
Xander Miller <xander.miller@gmail.com>
2016-08-11 23:01:49 +0000

    adds private topics to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,60 +2,69 @@ include RandomData
 
 #Create Users
 
-  5.times do
-    user = User.create!(
-      name:     RandomData.random_name,
-      email:    RandomData.random_email,
-      password: RandomData.random_sentence
-    )
-  end
-  users = User.all
-# Create topics
-  15.times do
-    Topic.create!(
-      name:         RandomData.random_sentence,
-      description:  RandomData.random_paragraph
-    )
-  end
-  topics = Topic.all
-
-
-# Create posts
-  50.times do
-
-    Post.create!(
-      user:  users.sample,
-      topic: topics.sample,
-      title: RandomData.random_sentence,
-      body:  RandomData.random_paragraph
-    )
-  end
-  posts = Post.all
-
-  #Create comments
-
-  100.times do
-    Comment.create!(
-    user: users.sample,
-      post: posts.sample,
-      body:  RandomData.random_paragraph
-    )
-  end
-
-  # Create an admin user
-  admin = User.create!(
+5.times do
+  user = User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+  
+# Create an admin user
+admin = User.create!(
   name:     'Admin User',
   email:    'admin@example.com',
   password: 'helloworld',
   role:     'admin'
-  )
+)
 
-  #Create a member
-  member = User.create!(
+#Create a member
+member = User.create!(
   name:     'Member User',
   email:    'member@example.com',
   password: 'helloworld'
+)
+
+users = User.all
+# Create topics
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
   )
+end
+
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph,
+    public: false
+  )
+end
+topics = Topic.all
+
+
+# Create posts
+50.times do
+
+  Post.create!(
+    user:  users.sample,
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body:  RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+#Create comments
+100.times do
+  Comment.create!(
+  user: users.sample,
+    post: posts.sample,
+    body:  RandomData.random_paragraph
+  )
+end
+
 
 puts "Seed finished"
 puts "#{User.count} users created."
